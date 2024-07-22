89b084670d876544e548e2bc258dbf1ccdd37e77
Jeffry Leon <ryffje@gmail.com>
2024-07-22 12:22:16 -0600

    feat(variables): add variables to the bank money withdraw example

--- fundamentals/variables/bank-money-withdraw.js
+++ fundamentals/variables/bank-money-withdraw.js
@@ -1,3 +1,5 @@
+function(bank)
+
 const bank = "Devlive"; // name with clear meaning
 const currency = "Colon"; // name with clear meaning
 let colonesAmount = 40000; // name with clear meaning, declare with let because it can change and his data type is a number
