35e709d2f677affc7ac6826532c1c1d3fcba9b0a
Jeffry Leon <ryffje@gmail.com>
2024-07-23 12:55:32 -0600

    feat(operators): add bmi exercise

--- fundamentals/operators/bmi.js
+++ fundamentals/operators/bmi.js
@@ -4,6 +4,7 @@ const name = prompt("What is your name? ");
 const height = parseFloat(prompt("What is your height in meters? "));
 const weight = parseFloat(prompt("What is your weight in kilograms? "));
 
+
 const bmi = weight / Math.pow(height, 2);
 
 console.log(`Hi ${name}, your BMI is ${bmi.toFixed(2)}`);
