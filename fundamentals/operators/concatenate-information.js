65dcbde706a7aad3bf0a2b29558b07d5b1f00357
Jeffry Leon <ryffje@gmail.com>
2024-07-23 12:30:27 -0600

    feat(operators): add concatenate information exercise

---
+++ fundamentals/operators/concatenate-information.js
@@ -0,0 +1,7 @@
+const firstName = "Mario"
+const lastName = "Bros"
+const Age = "16"
+const favoriteHobby = "swimming"
+const email = "mariobros@gmail.com"
+
+console.log("My full name is" + " " + firstName + " " + lastName + ". I'm" + " " + age + " " + "and my favorite hobby is" + " " + favoriteHobby + ". My email is:" + " " + email)
