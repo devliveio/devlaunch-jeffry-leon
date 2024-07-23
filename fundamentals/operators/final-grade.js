9e182bb39e2cd7b79452851ae1ece801d7996815
Jeffry Leon <ryffje@gmail.com>
2024-07-23 12:36:38 -0600

    feat(operators): add final grade exercise

---
+++ fundamentals/operators/final-grade.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')()
+
+const grade1 = parseFloat(prompt("Enter the first grade: "))
+const grade2 = parseFloat(prompt("Enter the second grade: "))
+const grade3 = parseFloat(prompt("Enter the third grade: "))
+
+const averageGrade = (grade1 + grade2 + grade3) / 3
+
+console.log(`Final average grade: ${averageGrade.toFixed(2)})
