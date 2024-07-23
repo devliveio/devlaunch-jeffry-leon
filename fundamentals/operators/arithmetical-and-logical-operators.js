69c4025a0a68b4755a3c23400bb22ddd2b2b0e46
Jeffry Leon <ryffje@gmail.com>
2024-07-23 12:18:35 -0600

    feat(operators): add the example for arithmetical and logical operators

---
+++ fundamentals/operators/arithmetical-and-logical-operators.js
@@ -0,0 +1,26 @@
+const dividend = 24 + 4 ** 2 -70 * 120
+const divider = 15
+
+const total = dividend / divider
+
+console.log('Result: ', total)
+
+
+const subjects = 3
+
+const susanaMatch = 10
+const susanaScience = 8
+const susanaHistory = 9
+
+const juanMatch = 9
+const juanScience = 10
+const juanHistory = 9
+
+const averageSusana = (susanaMatch + susanaScience + susanaHistory)
+const averageJuan = (juanMatch + juanScience + juanHistory)
+
+console.log ("Susana's average is: ", averageSusana)
+console.log ("Juan's average is ", averageJuan)
+
+console.log("Susana is better than Juan", averageSusana > averageJuan)
+
