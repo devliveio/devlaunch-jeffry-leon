21077b9aff3f85870f71294be0bb9408fe68dd6b
Jeffry Leon <ryffje@gmail.com>
2024-07-23 12:54:10 -0600

    feat(operators): add hotel exercise

---
+++ fundamentals/operators/bmi.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+const name = prompt("What is your name? ");
+const height = parseFloat(prompt("What is your height in meters? "));
+const weight = parseFloat(prompt("What is your weight in kilograms? "));
+
+const bmi = weight / Math.pow(height, 2);
+
+console.log(`Hi ${name}, your BMI is ${bmi.toFixed(2)}`);
