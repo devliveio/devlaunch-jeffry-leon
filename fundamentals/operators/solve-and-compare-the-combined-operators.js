aad553677f87d0c421b0e769248fdbf876722c96
Jeffry Leon <ryffje@gmail.com>
2024-07-24 09:23:56 -0600

    feat(operators): add solved and compare combined operators exercise

---
+++ fundamentals/operators/solve-and-compare-the-combined-operators.js
@@ -0,0 +1,9 @@
+const number1 = -1
+const number2 = 2/3
+const number3 = (2+3)/(5-7)
+
+const leftoperation = number1 + number2 + (number3 **2)
+const rightoperation = 1 - (2 ** (3 ** 5))
+
+console.log(leftoperation > rightoperation)
+
