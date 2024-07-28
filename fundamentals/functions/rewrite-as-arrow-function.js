d1a1fba3e3f28f4985721ca3f18685b61bab543a
Jeffry Leon <ryffje@gmail.com>
2024-07-27 18:12:27 -0600

    feat(functions): add rewrite as arrow functions file

---
+++ fundamentals/functions/rewrite-as-arrow-function.js
@@ -0,0 +1,23 @@
+const calculateTax = (price) => {
+  if (price > 50000) {
+      return price * 0.13 - price * 0.5
+    } else {
+     return price * 0.13
+  }
+}
+  
+  
+console.log(calculateTax(50000))
+
+const fibonacci = (n) => {
+    if (n < 2) {
+      return n
+    }
+    
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+console.log(fibonacci(10))
+
+
+
