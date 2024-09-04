49317ca0161990afd794de0770888c513ea2b6df
Jeffry Leon <ryffje@gmail.com>
2024-09-04 14:10:43 -0600

    feat(polymorphism): add restaurant file

---
+++ object-oriented-programming/polymorphism/restaurant.ts
@@ -0,0 +1,24 @@
+abstract class Restaurant {
+    abstract serve(): void
+}
+
+class FastFood extends Restaurant {
+    serve(){
+        console.log("Serving burgers and fries")
+        }}
+class FineDining extends Restaurant {
+    serve() {
+        console.log("Serving gourmet dishes")
+    }}
+class Cafeteria extends Restaurant {
+    serve() {
+        console.log("Serving a variety of options")
+    }}
+    
+    const fastFood = new FastFood()
+    fastFood.serve()
+    const fineDining = new FineDining()
+    fineDining.serve()
+    const cafeteria = new Cafeteria()
+    cafeteria.serve()
+
