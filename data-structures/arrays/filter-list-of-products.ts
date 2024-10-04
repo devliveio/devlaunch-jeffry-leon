b63cd7b866e1b3d8e098e551d9fac0af5cac34b5
Jeffry Leon <ryffje@gmail.com>
2024-10-04 13:35:28 -0600

    feat(arrays): add filter products file

--- data-structures/arrays/filter-list-of-products.ts
+++ data-structures/arrays/filter-list-of-products.ts
@@ -1,22 +1,27 @@
 type Product = {
-    name: string,
-    price: number,
-    inStock: boolean,
-    category: 'technology' | 'home' | 'clothing' | 'food';
+
+    name: string
+    price: number
+    inStock: boolean
+    category: `technology` | `home` | `clothing`| `food`;
 }
 
 const products: Product[] = [
-    { name: "TV", price: 400, inStock: true, category: 'technology' },
-    { name: "Microwave", price: 80, inStock: true, category: 'home' },
-    { name: "Pant", price: 35, inStock: false, category: 'clothing' },
-    { name: "Iphone", price: 110, inStock: true, category: 'technology' },
-    { name: "Protein", price: 30, inStock: true, category: 'food' },
-    { name: "Couch", price: 70, inStock: true, category: 'home' },
-] 
+    { name: "Laptop", price: 200, inStock: true, category: 'technology' },
+    { name: "Toaster", price: 45, inStock: true, category: 'home' },
+    { name: "Shirt", price: 35, inStock: false, category: 'clothing' },
+    { name: "Smartphone", price: 100, inStock: true, category: 'technology' },
+    { name: "Bread", price: 2, inStock: true, category: 'food' },
+    { name: "Lamp", price: 60, inStock: true, category: 'home' },
+]
 
-const filteredProducts: Product[] = products.filter(product => (product.inStock && 
-    (product.price >= 50 || product.price <= 150) && (product.category === 'technology' || product.category === 'home' )))
+const filterProducts: Product [] = products.filter(product => (
+    product.inStock &&
+    (product.price >= 50 && product.price <= 150) &&
+    (product.category === `technology` || product.category === `home`)
+))
 
-    console.log(filteredProducts)
+console.log(filterProducts)
+
 
 
