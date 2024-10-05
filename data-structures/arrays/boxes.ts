c78aee9295ba359d6fa3651e1c106936b4994144
Jeffry Leon <ryffje@gmail.com>
2024-10-05 08:14:43 -0600

    feat(arrays): add boxes file

--- data-structures/arrays/boxes.ts
+++ data-structures/arrays/boxes.ts
@@ -1,15 +1,13 @@
-const container: number [] = 
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const container: number[] =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const totalWeight: number = container.reduce((acc, weight) => acc + weight, 0);
-const averageWeight: number = totalWeight /container.length
+const totalWeight: number = container.reduce((accumulator, weight) => accumulator + weight, 0)
+const averageWeight: number = totalWeight / container.length 
 
-const boxesExceeding: number [] = container.filter(weight => weight > averageWeight)
-const countExceeding: number = boxesExceeding.length
+const exceedingAverage: number [] = container.filter(wight => wight > averageWeight)
+const countExceedingAverage: number = exceedingAverage.length
 
 console.log(averageWeight)
-console.log(countExceeding)
-
+console.log(countExceedingAverage)
 
 
 
