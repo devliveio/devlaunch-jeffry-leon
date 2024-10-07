be0abbd6dc19c03813cfc0f58b4414eea411124a
Jeffry Leon <ryffje@gmail.com>
2024-10-07 11:58:50 -0600

    feat(arrays): add mini arrays file

--- data-structures/arrays/mini-arrays-challenge.ts
+++ data-structures/arrays/mini-arrays-challenge.ts
@@ -1,32 +1,30 @@
-const testNumbers: number [] = 
-[20, 21, 22, 23, 24, 25]
-
-function isPrime(num: number): boolean {
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false;
+function isPrime (number: number): boolean {
+    for (let i = 2; i <= Math.sqrt(number); i++){
+        if(number % i === 0){
+            return false
         }
     }
 
     return true;
 }
 
-function smallestPrime(numbers: number[]):number | undefined {
-    let smallestPrime: number | undefined;
+console.log (isPrime)
 
+
+function smallestPrime(numbers: number[]): number | undefined {
+    let smallestPrime: number | undefined;
+  
     for (const num of numbers) {
-        if (isPrime(num)) {
-          if (smallestPrime === undefined || num < smallestPrime) {
-            smallestPrime = num
-          }
+      if (isPrime(num)) {
+        if (smallestPrime === undefined || num < smallestPrime) {
+          smallestPrime = num
         }
       }
-      return smallestPrime
     }
+    return smallestPrime
+  }
 
 
+const testNumbers: number [] = [20, 21, 22, 23, 24, 25]
 console.log(smallestPrime(testNumbers))
-
-
-
 
