b9cee8a8e313694e206d655e2fb9b3c1e0a8d9b1
Jeffry Leon <ryffje@gmail.com>
2024-10-08 13:07:22 -0600

    feat(arrays): add sort.number file

--- data-structures/arrays/sort-numbers.ts
+++ data-structures/arrays/sort-numbers.ts
@@ -1,23 +1,3 @@
-
-function sortNumbers (numbers: number[]): number[] {
-    const n: number = numbers.length
-
-    for (let i = 0; i < n - 1; i++){
-        for (let j = 0; j < n - i - 1; j++) {
-            if (numbers[j] > numbers[j + 1]) {
-              const temp = numbers[j]
-              numbers[j] = numbers[j + 1]
-              numbers[j + 1] = temp
-            }
-          }
-        }
-        return numbers
-      }
-    
-const numbers: number [] = [1, 10, 4, 7, 6, 3]
-console.log("Original numbers:", numbers)
-const sortedNumbers = sortNumbers(numbers)
-console.log("Sorted numbers:", sortedNumbers)
-    
-
-
+let numSeries = [1, 10, 4, 7, 6, 3]
+numSeries.sort((a,b) => a - b)
+console.log("Original numbers [1,10,4,7,6,3]", ` Sorted numbers:[${numSeries}]`)
