815f1b82a8523542ea20ce427686f3dc491ab11c
Jeffry Leon <ryffje@gmail.com>
2024-10-08 17:23:57 -0600

    feat(arrays): add sum arrays file

--- data-structures/arrays/sum-arrays.ts
+++ data-structures/arrays/sum-arrays.ts
@@ -1,22 +1,33 @@
+function Arrays_sum(arr1: number[], arr2: number[]): number [] 
+{
+  var result: number[] = [];
+  var ctr = 0;
+  var x=0;
+
+ while (ctr < arr1.length && ctr < arr2.length) 
+  {
+    result.push(arr1[ctr] + arr2[ctr]);
+    ctr++;
+  }
+
+ if (ctr === arr1.length) 
+ {
+    for (x = ctr; x < arr2.length; x++)   {
+      result.push(arr2[x]);
+    }
+  } 
+  else
+  {
+  for (x = ctr; x < arr1.length; x++) 
+    {
+      result.push(arr1[x]);
+    }
+  }
+  return result;
+}
+
 const arr1: number[] = [1, 0, 2, 3, 4]
 const arr2: number[] = [3, 5, 6, 7, 8, 13, 9]
-
-
-function sumArrays(arr1: number[], arr2: number[]) {
-    const result: number [] = []
-    const maxLength = Math.max(arr1.length, arr2.length)
-    
-    for(let i = 0; i < maxLength; i++){
-        const value1 = arr1[i] || 0
-        const value2 = arr2[i] || 0
-        result.push(value1 + value2)
-      
-}
-
-    return result;
-
-}
-const sumArray = sumArrays (arr1, arr2)
-console.log(sumArray)
+console.log(Arrays_sum(arr1, arr2))
 
 
